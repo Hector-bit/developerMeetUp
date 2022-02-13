ff2d7bb5c6356da2c0c02790ee72a8f0349095ea
Hector-bit <hectormartinez.2978@gmail.com>
2022-02-13 06:34:33 -0800

    views addition

--- website_api/api/views.py
+++ website_api/api/views.py
@@ -3,23 +3,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from api.serializers import CreateProjectSerializer, UserSerializer
 from .models import User, Project
-# from rest_framework import generics
-# from .serializers import RoomSerializer
-# from .models import Room
 
 # Create your views here.
-
-
 class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-# class ProjectView(generics.ListAPIView):
-#     queryset = Projects.objects.all()
-#     serializer_class = ProjectSerializer
-
-# class CreateProject(generics.CreateAPIView):
-#     serializer_project = CreateProjectSerializer
-
-    # def post(self, request, format=None):
-    #     if not 
