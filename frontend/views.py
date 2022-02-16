621cc30cb358e5093b2e8ac0fefad44d866a014b
Hector-bit <hectormartinez.2978@gmail.com>
2022-02-16 04:30:49 -0800

    sign up page

--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,11 @@
+from curses import ALL_MOUSE_EVENTS
 from django.shortcuts import render
 from .models import User, Project
 
 # Create your views here.
 def home(request):
-    all_users = User.objects.all
-    return render(request, "home.html", {'all': all_users})
+    all_projects = Project.objects.all
+    return render(request, "home.html", {'all': all_projects})
 
+def join(request):
+    return render (request, 'join.html', {})
